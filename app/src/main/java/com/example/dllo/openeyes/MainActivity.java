66378a182ba20c4a083f9ed430c00470d5697bbc
JTianyi <452436674@qq.com>
2016-08-12 09:38:11 +0800

    初始化工程

---
+++ app/src/main/java/com/example/dllo/openeyes/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dllo.openeyes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
